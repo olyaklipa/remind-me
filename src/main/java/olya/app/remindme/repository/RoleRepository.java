b4d14b0ea6ead174c77c6ac2f029d438f8a66e6a
olyaklipa <olyaklipa@gmail.com>
2023-07-02 19:33:22 +0300

    @Repository annotation was added

--- src/main/java/olya/app/remindme/repository/RoleRepository.java
+++ src/main/java/olya/app/remindme/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package olya.app.remindme.repository;
 
 import olya.app.remindme.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/olya/app/remindme/repository/SubjectRepository.java
+++ src/main/java/olya/app/remindme/repository/SubjectRepository.java
@@ -2,6 +2,8 @@ package olya.app.remindme.repository;
 
 import olya.app.remindme.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SubjactRepository extends JpaRepository<Subject, Long> {
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
 }
