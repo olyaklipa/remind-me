9ec529b22e2179174bfa42a2a81fb9e3020a43cd
olyaklipa <olyaklipa@gmail.com>
2023-07-03 00:12:57 +0300

    UserController: registration implemented

---
+++ src/main/java/olya/app/remindme/controller/RoleController.java
@@ -0,0 +1,29 @@
+package olya.app.remindme.controller;
+
+import olya.app.remindme.model.Role;
+import olya.app.remindme.service.RoleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+    private RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String populateRoles(){
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        return "Roles added!";
+    }
+
+}
